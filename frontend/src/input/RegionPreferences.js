b03d8be1b51d1727383411ac0102de6c164c933b
Bogdan Putineanu <94795061+197Bogdan@users.noreply.github.com>
2023-11-12 01:47:08 +0200

    added slider tooltips

--- frontend/src/input/RegionPreferences.js
+++ frontend/src/input/RegionPreferences.js
@@ -30,11 +30,11 @@ const RegionPreferences = () => {
                 justifyContent: 'space-around'
                 }}
             >
-                <ScoreSlider name="Cost of Living"/>
-                <ScoreSlider name="Education"/>
-                <ScoreSlider name="Safety"/>
-                <ScoreSlider name="Outdoors"/>
-                <ScoreSlider name="Environmental Quality"/>
+                <ScoreSlider name="Cost of Living" tooltip="Price of average consumer basket."/>
+                <ScoreSlider name="Education" tooltip="Best local university, average student results."/>
+                <ScoreSlider name="Safety" tooltip="Crime rate, guns per capita."/>
+                <ScoreSlider name="Outdoors" tooltip="Presence of hills, mountains and water access."/>
+                <ScoreSlider name="Environmental Quality" tooltip="Cleanliness, air and water quality."/>
             </Box>
 
             <Button 
@@ -51,18 +51,18 @@ const RegionPreferences = () => {
                 justifyContent: 'space-around'
                 }}
             >
-                <ScoreSlider name="Economy"/>
-                <ScoreSlider name="Housing" tooltip="Something something something"/>
-                <ScoreSlider name="Startups"/>
-                <ScoreSlider name="Venture Capital"/> 
-                <ScoreSlider name="Travel Connectivity"/>
-                <ScoreSlider name="Commute"/> 
-                <ScoreSlider name="Businenss Freedom"/>
-                <ScoreSlider name="Healthcare"/> 
-                <ScoreSlider name="Taxation"/>
-                <ScoreSlider name="Internet Access"/>
-                <ScoreSlider name="Leisure & Culture"/> 
-                <ScoreSlider name="Tolerance"/>
+                <ScoreSlider name="Economy" tooltip="GDP per capita, growth rate."/>
+                <ScoreSlider name="Housing" tooltip="Average rent price per month."/>
+                <ScoreSlider name="Startups" tooltip="Presence of new startups, investors and events."/>
+                <ScoreSlider name="Venture Capital" tooltip="Venture capital."/> 
+                <ScoreSlider name="Travel Connectivity" tooltip="Connection to trains and airports."/>
+                <ScoreSlider name="Commute" tooltip="Quality of traffic."/> 
+                <ScoreSlider name="Businenss Freedom" tooltip="Business freedom from corruption."/>
+                <ScoreSlider name="Healthcare" tooltip="Healthcare quality and life expectancy."/> 
+                <ScoreSlider name="Taxation" tooltip="Taxation."/>
+                <ScoreSlider name="Internet Access" tooltip="Internet download and upload speed."/>
+                <ScoreSlider name="Leisure & Culture" tooltip="Museums, cinemas, galleries, concerts etc."/> 
+                <ScoreSlider name="Tolerance" tooltip="Acceptance of minorities, LGBT rights."/>
             </Box>
         </Box>
     </>
